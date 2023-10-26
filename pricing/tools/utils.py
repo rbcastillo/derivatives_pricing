f0c1293643de8153b656ef4fd11889d6b9e65bf0
Ruben Castillo Sanchez <46053423+rbcastillo@users.noreply.github.com>
2023-10-26 09:07:48 +0200

    [FEATURE] Added auxiliary class StatisticalProcess and associated tests

--- pricing/tools/utils.py
+++ pricing/tools/utils.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import Tuple
 
 
 class FinancialProduct(ABC):
@@ -50,6 +51,118 @@ class FinancialProduct(ABC):
         """
         This method allows updating some or all of the relevant input for the object. The kwargs used in the
         method must match those in the __init__ method.
+
+        :param kwargs: keyword parameter(s) and value(s) to be updated.
+        :return: None.
+        """
+        for param, value in kwargs.items():
+            setattr(self, param, value)
+
+
+class StatisticalProcess(ABC):
+    """
+    Base object to implement the main methods to be shared across all statistical processes implementations.
+    """
+
+    __slots__ = ['size']
+
+    @abstractmethod
+    def __init__(self, size: Tuple[int, ...]) -> None:
+        """
+        Abstract method for the initialization of each specific object based on its implementation. The size
+        attribute, needed across all statistical processes is loaded in this base method.
+
+        :param size:
+        """
+        self.size = self._manage_size(size)
+
+    @staticmethod
+    def _manage_size(size: Tuple[int, ...]) -> Tuple[int, ...]:
+        """
+        Method to ensure that the size parameter is compliant with the logic of the process. The size parameter should
+        meet the following criteria:
+
+        - Length: the length of the parameter should be between 1 and 3 inclusive for it to have financial sense. The
+          first dimension represents the number of time steps, the second the number of different paths and the third
+          the number of asset simulations.
+        - Zeros: the size tuple should not have zeros in the intermediate places, zeros at the end are removed reducing
+          the length of the parameter.
+
+        :param size: tuple containing the dimensions of the output statistical process.
+        :return: adjusted tuple if needed, otherwise the original tuple is returned.
+        """
+        StatisticalProcess._manage_size_length(size)
+        size = StatisticalProcess._manage_size_zeros(size)
+        return size
+
+    @staticmethod
+    def _manage_size_length(size: Tuple[int, ...]) -> None:
+        """
+        Auxiliary method to ensure that the length of the size parameter is compliant with the logic of the process.
+        The length of the parameter should be between 1 and 3 inclusive for it to have financial sense. The first
+        dimension represents the number of time steps, the second the number of different paths and the third
+        the number of asset simulations.
+
+        :param size: tuple containing the dimensions of the output statistical process.
+        :return: None.
+        """
+        if len(size) == 0:
+            raise ValueError(f'Size <{size}> not valid, at least should have one dimension')
+        elif len(size) > 3:
+            raise ValueError(f'Size <{size}> not valid, maximum number of dimensions is 3')
+
+    @staticmethod
+    def _manage_size_zeros(size: Tuple[int, ...]) -> Tuple[int, ...]:
+        """
+        Method to ensure that the content of the size parameter is compliant with the logic of the process. The size
+        tuple should not have zeros in the intermediate places, zeros at the end are removed reducing the length of
+        the parameter.
+
+        :param size: tuple containing the dimensions of the output statistical process.
+        :return: adjusted tuple if needed, otherwise the original tuple is returned.
+        """
+        if 0 in size:
+            if size[-1] == 0:
+                size = StatisticalProcess._manage_size(size[:-1])
+            else:
+                raise ValueError(f'Size <{size}> not valid, zero length inner dimensions are not allowed')
+        return size
+
+    def __str__(self) -> str:
+        """
+        Method to create a string representation of the object. This method outputs the object name and the
+        relevant parameters with their values.
+
+        :return: string representation of the object.
+        """
+        attr_values = {attr: getattr(self, attr) for attr in self.__slots__ if not attr.startswith('_')}
+        text_output = f'{self.__class__.__name__} object with parameters {attr_values}\n{self.__doc__}'
+        return text_output
+
+    def __setattr__(self, key, value) -> None:
+        """
+        Method to manage the process of setting and updating object parameters. Only attributes declared in the
+        __init__ method that are not protected or private are allowed to be updated. This avoids adding
+        new attributes or modifying attributes that should only be modified inside the object as a result of
+        certain operations.
+
+        :param key: parameter to be added or updated.
+        :param value: value to be assigned to the parameter.
+        :return: None
+        """
+        if key not in self.__slots__ or key.startswith('_'):
+            valid_attrs = [attr for attr in self.__slots__ if not attr.startswith('_')]
+            if key not in self.__slots__:
+                raise ValueError(f'Attribute name <{key}> is not recognized, use values in {valid_attrs}')
+            if key.startswith('_'):
+                raise ValueError(f'Attribute <{key}> is protected or private, use values in {valid_attrs}')
+        else:
+            object.__setattr__(self, key, value)
+
+    def update_params(self, **kwargs) -> None:
+        """
+        This method allows updating some or all of the relevant input for the European option class. The kwargs used
+        in the method must match those in the __init__ method.
 
         :param kwargs: keyword parameter(s) and value(s) to be updated.
         :return: None.
--- tests/test_tools/test_tools.py
+++ tests/test_tools/test_tools.py
@@ -2,6 +2,7 @@ import unittest
 import numpy as np
 
 from pricing.tools.helper import Balance
+from pricing.tools.utils import StatisticalProcess
 
 
 class TestBalance(unittest.TestCase):
@@ -39,6 +40,49 @@ class TestBalance(unittest.TestCase):
         discounted_future_value = Balance(amount=future_value).discount(r=r, t=t, compounding=compounding)
         balance_matching = np.isclose(discounted_future_value, start_value)
         self.assertTrue(balance_matching)
+
+
+class TestStatisticalProcess(unittest.TestCase):
+
+    def test_no_size(self):
+        try:
+            StatisticalProcess._manage_size(size=())
+            self.assertTrue(False)
+        except ValueError as error:
+            expected = ValueError(f'Size <()> not valid, at least should have one dimension')
+            self.assertTrue(type(error) is type(expected) and error.args == expected.args)
+
+    def test_size_too_long(self):
+        try:
+            StatisticalProcess._manage_size(size=(1, 2, 3, 4))
+            self.assertTrue(False)
+        except ValueError as error:
+            expected = ValueError(f'Size <(1, 2, 3, 4)> not valid, maximum number of dimensions is 3')
+            self.assertTrue(type(error) is type(expected) and error.args == expected.args)
+
+    def test_size_with_inner_zero(self):
+        try:
+            StatisticalProcess._manage_size(size=(10, 0, 10))
+            self.assertTrue(False)
+        except ValueError as error:
+            expected = ValueError(f'Size <(10, 0, 10)> not valid, zero length inner dimensions are not allowed')
+            self.assertTrue(type(error) is type(expected) and error.args == expected.args)
+
+    def test_size_with_single_zero_end(self):
+        size = StatisticalProcess._manage_size(size=(10, 10, 0))
+        self.assertTrue(size == (10, 10))
+
+    def test_size_with_double_zero_end(self):
+        size = StatisticalProcess._manage_size(size=(10, 0, 0))
+        self.assertTrue(size == (10, ))
+
+    def test_size_with_all_zeros(self):
+        try:
+            StatisticalProcess._manage_size(size=(0, 0, 0))
+            self.assertTrue(False)
+        except ValueError as error:
+            expected = ValueError(f'Size <()> not valid, at least should have one dimension')
+            self.assertTrue(type(error) is type(expected) and error.args == expected.args)
 
 
 if __name__ == '__main__':
