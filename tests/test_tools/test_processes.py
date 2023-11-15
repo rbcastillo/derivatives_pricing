797cd3c93763930bf7fffdc9b7870d51ce0460d6
Ruben Castillo Sanchez <46053423+rbcastillo@users.noreply.github.com>
2023-11-15 23:33:08 +0100

    [REFACTOR] Improved tests suite for statistical processes

--- tests/test_tools/test_processes.py
+++ tests/test_tools/test_processes.py
@@ -1,176 +1,117 @@
 import unittest
-from math import sqrt, exp
 
 import numpy as np
 
 from pricing.simulation.processes import GaussianWhiteNoise, Wiener, GeometricBrownianMotion
-from tests.env_variables import LOW_TOL, HIGH_TOL
+from tests.test_tools.helper import HelperGaussianWhiteNoise, HelperWiener, HelperGBM
 
 
 class TestGaussianWhiteNoise(unittest.TestCase):
 
-    def test_gaussian_white_noise_1d(self) -> None:
-        size = (500_000, )
-        sigma = .5
-        process = GaussianWhiteNoise(size=size, sigma=sigma).generate()
-        shape_test = process.shape == size
-        mean_test = np.isclose(process.mean(), 0., atol=LOW_TOL)
-        std_test = np.isclose(process.std(), sigma, atol=LOW_TOL)
-        self.assertTrue(shape_test and mean_test and std_test)
-
     def test_gaussian_white_noise_2d(self) -> None:
-        size = (100_000, 1_000)
-        sigma = .5
+        np.random.seed(42)
+        size, sigma = (1_000, 100_000), .5
         process = GaussianWhiteNoise(size=size, sigma=sigma).generate()
-        shape_test = process.shape == size
-        mean_test = np.isclose(process.mean(), 0., atol=LOW_TOL)
-        std_test = np.isclose(process.std(axis=0).mean(), sigma, atol=LOW_TOL)
-        self.assertTrue(shape_test and mean_test and std_test)
+        self.assertTrue(all(HelperGaussianWhiteNoise.calc_tests(process, size, mu=0, sigma=sigma)))
 
     def test_gaussian_white_noise_3d_single_sigma(self) -> None:
-        size = (100_000, 1_000, 3)
-        sigma = .5
+        np.random.seed(42)
+        size, sigma = (250, 100_000, 3), .5
         process = GaussianWhiteNoise(size=size, sigma=sigma).generate()
-        shape_test = process.shape == size
-        mean_test = np.isclose(process.mean(), 0., atol=LOW_TOL)
-        std_test = np.allclose(process.std(axis=0).mean(axis=0), np.array([sigma] * size[2]), atol=LOW_TOL)
-        self.assertTrue(shape_test and mean_test and std_test)
+        self.assertTrue(all(HelperGaussianWhiteNoise.calc_tests(process, size, mu=0, sigma=sigma)))
 
     def test_gaussian_white_noise_3d_vector_sigma(self) -> None:
-        size = (100_000, 1_000, 3)
-        sigma = [.25, .5, 1.]
+        np.random.seed(42)
+        size, sigma = (250, 100_000, 3), [.25, .5, 1.]
         process = GaussianWhiteNoise(size=size, sigma=sigma).generate()
-        shape_test = process.shape == size
-        mean_test = np.isclose(process.mean(), 0., atol=LOW_TOL)
-        std_test = np.allclose(process.std(axis=0).mean(axis=0), np.array(sigma), atol=LOW_TOL)
-        self.assertTrue(shape_test and mean_test and std_test)
+        self.assertTrue(all(HelperGaussianWhiteNoise.calc_tests(process, size, mu=0, sigma=sigma)))
+
+    def test_consistency(self) -> None:
+        np.random.seed(42)
+        size, sigma = (250, 100_000, 3), [.25, .5, 1.]
+        process = GaussianWhiteNoise(size=size, sigma=sigma).generate()
+        self.assertTrue(process[-1].mean().round(9), 0.002093015)
 
 
 class TestWiener(unittest.TestCase):
 
     def test_wiener_2d(self) -> None:
-        periods = 12
-        num_periods = 10
-        size = (periods * num_periods, 500_000)
-        process = Wiener(size=size, periods=periods).generate()
-        shape_test = process.shape == (size[0] + 1, ) + size[1:]
-        mean_test = np.isclose(process[-1, :].mean(), 0., atol=LOW_TOL)
-        std_test = np.isclose(process[-1, :].std(axis=0).mean(), sqrt(num_periods), atol=LOW_TOL)
-        self.assertTrue(shape_test and mean_test and std_test)
+        np.random.seed(42)
+        time_units, sub_periods, num_paths = 4, 12, 100_000
+        size = (time_units * sub_periods, num_paths)
+        w = Wiener(size=(time_units * sub_periods, 250), sub_periods=sub_periods)
+        process = Wiener(size=size, sub_periods=sub_periods).generate()
+        self.assertTrue(all(HelperWiener.calc_tests(w, process, size, time_units, 250)))
 
     def test_wiener_3d_no_correlation(self) -> None:
-        periods = 12
-        num_periods = 10
-        num_assets = 3
-        size = (periods * num_periods, 500_000, num_assets)
-        process = Wiener(size=size, periods=periods).generate()
-        shape_test = process.shape == (size[0] + 1, ) + size[1:]
-        mean_test = np.isclose(process[-1, :].mean(), 0., atol=LOW_TOL)
-        std_test = np.isclose(process[-1, :].std(axis=0).mean(), sqrt(num_periods), atol=LOW_TOL)
-        correlations = np.corrcoef(process.reshape(-1, process.shape[-1]).T)
-        corr_test = np.allclose(correlations, np.identity(n=num_assets), atol=LOW_TOL)
-        self.assertTrue(shape_test and mean_test and std_test and corr_test)
+        np.random.seed(42)
+        time_units, sub_periods, num_paths, num_assets = 4, 12, 100_000, 3
+        size = (time_units * sub_periods, num_paths, num_assets)
+        w = Wiener(size=(time_units * sub_periods, 250, num_assets), sub_periods=sub_periods)
+        process = Wiener(size=size, sub_periods=sub_periods).generate()
+        self.assertTrue(all(HelperWiener.calc_tests(w, process, size, time_units, num_paths=250)))
 
     def test_wiener_3d_with_correlation(self) -> None:
-        periods = 12
-        num_periods = 10
-        num_assets = 3
-        size = (periods * num_periods, 500_000, num_assets)
+        np.random.seed(42)
+        time_units, sub_periods, num_paths, num_assets = 4, 12, 100_000, 3
+        size = (time_units * sub_periods, num_paths, num_assets)
         rho = np.array([[1., .25, .5], [.25, 1., .75], [.5, .75, 1.]])
-        process = Wiener(size=size, rho=rho, periods=periods).generate()
-        shape_test = process.shape == (size[0] + 1, ) + size[1:]
-        mean_test = np.isclose(process[-1, :].mean(), 0., atol=LOW_TOL)
-        std_test = np.allclose(process[-1, :].std(axis=0), sqrt(num_periods), atol=LOW_TOL)
-        correlations = np.corrcoef(process.reshape(-1, process.shape[-1]).T)
-        corr_test = np.allclose(correlations, rho, atol=LOW_TOL)
-        self.assertTrue(shape_test and mean_test and std_test and corr_test)
+        w = Wiener(size=(time_units * sub_periods, 250, num_assets), rho=rho, sub_periods=sub_periods)
+        process = Wiener(size=size, rho=rho, sub_periods=sub_periods).generate()
+        self.assertTrue(all(HelperWiener.calc_tests(w, process, size, time_units, num_paths=250, rho=rho)))
+
+    def test_consistency(self) -> None:
+        np.random.seed(42)
+        time_units, sub_periods, num_paths, num_assets = 4, 12, 100_000, 3
+        size = (time_units * sub_periods, num_paths, num_assets)
+        rho = np.array([[1., .25, .5], [.25, 1., .75], [.5, .75, 1.]])
+        process = Wiener(size=size, rho=rho, sub_periods=sub_periods).generate()
+        self.assertTrue(process[-1].mean().round(9), -0.000743865)
 
 
 class TestGeometricBrownianMotion(unittest.TestCase):
 
     def test_gbm_2d(self) -> None:
-        periods = 12
-        num_periods = 5
-        size = (periods * num_periods, 500_000)
-        s0, mu, sigma, q = 100, .15, .20, .05
-        process = GeometricBrownianMotion(size=size, s0=s0, mu=mu, sigma=sigma, q=q, periods=periods).generate()
-        shape_test = process.shape == (size[0] + 1, ) + size[1:]
-        expected_mean = s0 * np.exp((mu - q) * num_periods)
-        mean_test = np.isclose(process[-1, :].mean(), expected_mean, atol=HIGH_TOL)
-        expected_std = sqrt(s0 ** 2 * exp(2 * (mu - q) * num_periods) * (exp(num_periods * sigma ** 2) - 1))
-        std_test = np.isclose(process[-1, :].std(axis=0).mean(), expected_std, atol=HIGH_TOL)
-        returns = process[1:, :] / process[:-1, :] - 1
-        performance = process[-1, :] / process[0, :] - 1
-        mu_test = np.isclose(performance.mean(), exp((mu - q) * num_periods) - 1, atol=LOW_TOL)
-        vol_test = np.isclose(returns.std(axis=0).mean() * sqrt(periods), sigma, atol=LOW_TOL)
-        self.assertTrue(shape_test and mean_test and std_test and mu_test and vol_test)
+        np.random.seed(42)
+        time_units, sub_periods, num_paths = 4, 12, 100_000
+        size = (time_units * sub_periods, num_paths)
+        params = {'s0': 100, 'mu': .15, 'sigma': .20, 'q': .05}
+
+        gbm = GeometricBrownianMotion(size=(time_units * sub_periods, 250), **params, sub_periods=sub_periods)
+        process = GeometricBrownianMotion(size=size, **params, sub_periods=sub_periods).generate()
+        self.assertTrue(all(HelperGBM.calc_tests(gbm, process, size, time_units, 250, **params)))
 
     def test_wiener_3d_no_correlation(self) -> None:
-        periods = 12
-        num_periods = 5
-        num_assets = 3
-        size = (periods * num_periods, 500_000, num_assets)
-        s0, mu, sigma, q = 100, .15, .20, .05
-        process = GeometricBrownianMotion(size=size, s0=s0, mu=mu, sigma=sigma, q=q, periods=periods).generate()
-        shape_test = process.shape == (size[0] + 1, ) + size[1:]
-        expected_mean = s0 * np.exp((mu - q) * num_periods)
-        mean_test = np.isclose(process[-1, :].mean(), expected_mean, atol=HIGH_TOL)
-        expected_std = sqrt(s0 ** 2 * exp(2 * (mu - q) * num_periods) * (exp(num_periods * sigma ** 2) - 1))
-        std_test = np.isclose(process[-1, :].std(axis=0).mean(), expected_std, atol=HIGH_TOL)
-        returns = process[1:, :, :] / process[:-1, :, :] - 1
-        performance = process[-1, :] / process[0, :] - 1
-        mu_test = np.isclose(performance.mean(), exp((mu - q) * num_periods) - 1, atol=LOW_TOL)
-        vol_test = np.isclose(returns.std(axis=0).mean() * sqrt(periods), sigma, atol=LOW_TOL)
-        correlations = np.corrcoef(returns.reshape(-1, returns.shape[-1]).T)
-        corr_test = np.allclose(correlations, np.identity(n=num_assets), atol=LOW_TOL)
-        self.assertTrue(shape_test and mean_test and std_test and mu_test and vol_test and corr_test)
+        np.random.seed(42)
+        time_units, sub_periods, num_paths, n_assets = 4, 12, 100_000, 3
+        size = (time_units * sub_periods, num_paths, n_assets)
+        params = {'s0': 100, 'mu': .15, 'sigma': .20, 'q': .05}
+
+        gbm = GeometricBrownianMotion(size=(time_units * sub_periods, 250, n_assets), **params, sub_periods=sub_periods)
+        process = GeometricBrownianMotion(size=size, **params, sub_periods=sub_periods).generate()
+        self.assertTrue(all(HelperGBM.calc_tests(gbm, process, size, time_units, 250, **params)))
 
     def test_wiener_3d_with_correlation(self) -> None:
-        periods = 12
-        num_periods = 5
-        num_assets = 3
-        size = (periods * num_periods, 500_000, num_assets)
-        s0, mu, sigma, q = 100, .15, .20, .05
-        rho = np.array([[1., .25, .5], [.25, 1., .75], [.5, .75, 1.]])
         np.random.seed(42)
-        process = GeometricBrownianMotion(size=size, s0=s0, mu=mu, sigma=sigma, q=q, rho=rho,
-                                          periods=periods).generate()
-        shape_test = process.shape == (size[0] + 1, ) + size[1:]
-        expected_mean = s0 * np.exp((mu - q) * num_periods)
-        mean_test = np.isclose(process[-1, :].mean(), expected_mean, atol=1)
-        expected_std = sqrt(s0 ** 2 * exp(2 * (mu - q) * num_periods) * (exp(num_periods * sigma ** 2) - 1))
-        std_test = np.isclose(process[-1, :].std(axis=0).mean(), expected_std, atol=1)
-        returns = process[1:, :, :] / process[:-1, :, :] - 1
-        performance = process[-1, :] / process[0, :] - 1
-        mu_test = np.isclose(performance.mean(), exp((mu - q) * num_periods) - 1, atol=LOW_TOL)
-        vol_test = np.isclose(returns.std(axis=0).mean() * sqrt(periods), sigma, atol=LOW_TOL)
-        correlations = np.corrcoef(returns.reshape(-1, returns.shape[-1]).T)
-        corr_test = np.allclose(correlations, rho, atol=LOW_TOL)
-        self.assertTrue(shape_test and mean_test and std_test and mu_test and vol_test and corr_test)
+        time_units, sub_periods, num_paths, n_assets = 4, 12, 100_000, 3
+        size = (time_units * sub_periods, num_paths, n_assets)
+        params = {'s0': 100, 'mu': .15, 'sigma': .20, 'q': .05,
+                  'rho': np.array([[1., .25, .5], [.25, 1., .75], [.5, .75, 1.]])}
+
+        gbm = GeometricBrownianMotion(size=(time_units * sub_periods, 250, n_assets), **params, sub_periods=sub_periods)
+        process = GeometricBrownianMotion(size=size, **params, sub_periods=sub_periods).generate()
+        self.assertTrue(all(HelperGBM.calc_tests(gbm, process, size, time_units, 250, **params)))
 
     def test_wiener_3d_with_correlation_vector_variables(self) -> None:
-        periods = 12
-        num_periods = 5
-        num_assets = 3
-        size = (periods * num_periods, 500_000, num_assets)
-        s0, mu, sigma, q = [100, 150, 50], [.15, .10, .05], [.20, .10, .15], [.05, .02, .01]
-        rho = np.array([[1., .25, .5], [.25, 1., .75], [.5, .75, 1.]])
         np.random.seed(42)
-        process = GeometricBrownianMotion(size=size, s0=s0, mu=mu, sigma=sigma, q=q, rho=rho,
-                                          periods=periods).generate()
-        s0, mu, sigma, q = np.array(s0), np.array(mu), np.array(sigma), np.array(q)
-        shape_test = process.shape == (size[0] + 1, ) + size[1:]
-        expected_mean = s0 * np.exp((mu - q) * num_periods)
-        mean_test = np.allclose(process[-1, :].mean(axis=0), expected_mean, atol=1)
-        expected_std = np.sqrt(s0 ** 2 * np.exp(2 * (mu - q) * num_periods) * (np.exp(num_periods * sigma ** 2) - 1))
-        std_test = np.allclose(process[-1, :].std(axis=0), expected_std, atol=1)
-        returns = process[1:, :, :] / process[:-1, :, :] - 1
-        performance = process[-1, :] / process[0, :] - 1
-        mu_test = np.allclose(performance.mean(axis=0), np.exp((mu - q) * num_periods) - 1, atol=LOW_TOL)
-        vol_test = np.allclose(returns.std(axis=0).mean(axis=0) * sqrt(periods), sigma, atol=LOW_TOL)
-        correlations = np.corrcoef(returns.reshape(-1, returns.shape[-1]).T)
-        corr_test = np.allclose(correlations, rho, atol=LOW_TOL)
-        self.assertTrue(shape_test and mean_test and std_test and mu_test and vol_test and corr_test)
+        time_units, sub_periods, num_paths, n_assets = 4, 12, 100_000, 3
+        size = (time_units * sub_periods, num_paths, n_assets)
+        params = {'s0': [100, 150, 50], 'mu': [.15, .10, .05], 'sigma': [.20, .10, .15], 'q': [.05, .02, .01],
+                  'rho': np.array([[1., .25, .5], [.25, 1., .75], [.5, .75, 1.]])}
+
+        gbm = GeometricBrownianMotion(size=(time_units * sub_periods, 250, n_assets), **params, sub_periods=sub_periods)
+        process = GeometricBrownianMotion(size=size, **params, sub_periods=sub_periods).generate()
+        self.assertTrue(all(HelperGBM.calc_tests(gbm, process, size, time_units, 250, **params)))
 
 
 if __name__ == '__main__':
